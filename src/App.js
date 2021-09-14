5701ef95c429dd35ee64f5cc8cfa99566f46a8af
Ethan Maxwell <e.maxwell18p@gmail.com>
2021-09-14 14:17:00 -0400

    Section 3: Complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
-import TestItem from "./components/TestItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -26,28 +25,8 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
-      <TestItem></TestItem>
+      <h1>Let's get started!</h1>
+      <Expenses items={expenses} />
     </div>
   );
 }

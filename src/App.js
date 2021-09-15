4124abea8aff3771b80a046d83082c0fca442e7a
Ethan Maxwell <e.maxwell18p@gmail.com>
2021-09-15 11:10:46 -0400

    Test Buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Expenses from "./components/Expenses/Expenses";
+import Expenses from "./components/Expenses/Expenses"
 
 function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
+      title: "Toilet paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
